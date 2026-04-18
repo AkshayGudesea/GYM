f9f585d8ba3300186349ed500c71d91360e600d6
TejaswiniDavu <147189876+TejaswiniDavu@users.noreply.github.com>
2026-04-18 19:46:17 +0100

    first commit

---
+++ src/main/java/com/flc/booking/BookingStatus.java
@@ -0,0 +1,8 @@
+package main.java;
+
+public enum BookingStatus {
+    BOOKED,
+    CHANGED,
+    ATTENDED,
+    CANCELLED
+}
---
+++ src/main/java/com/flc/booking/ExerciseType.java
@@ -0,0 +1,9 @@
+package main.java;
+
+public enum ExerciseType {
+    YOGA,
+    ZUMBA,
+    AQUACISE,
+    BOX_FIT,
+    BODY_BLITZ
+}
